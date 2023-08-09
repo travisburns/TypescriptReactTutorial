d8fde6ebc0029a23e56f25ccc6241b2749065d5f
Travis <travisjb0@gmail.com>
2023-08-08 20:16:24 -0700

    Added Greet Component and props with Typescript

---
+++ reactTypescriptTutorial/src/components/Greet.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type GreetProps = {
+    name: string
+}
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>Welcome {props.name}! You have 10 unread messages</h2>
+    </div>
+  )
+}
+
+export default Greet
