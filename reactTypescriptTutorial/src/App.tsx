07a8e0754baba8b40f2f028284b4c6a2b3cea037
Travis <travisjb0@gmail.com>
2023-08-11 16:59:55 -0700

    Learned prop styling

--- reactTypescriptTutorial/src/App.tsx
+++ reactTypescriptTutorial/src/App.tsx
@@ -10,6 +10,7 @@ import Heading from './components/Heading'
 import Oscar from './components/Oscar'
 import Button from './components/Button'
 import Input from './components/Input'
+import Container from './components/Container'
 
 function App() {
   const personName = {
@@ -42,6 +43,7 @@ function App() {
       }}
       />
       <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles={{border: '1px solid black', padding: '1rem'}} />
     </div>
   )
 }
