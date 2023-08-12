b1364f85d7c6ea7899bdc46323d4e326eea35fd3
Travis <travisjb0@gmail.com>
2023-08-11 17:14:20 -0700

    Learned useState hook in typescript

---
+++ reactTypescriptTutorial/src/components/state/LoggedIn.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useState} from 'react'
+
+const LoggedIn = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false) 
+
+ const handleLogin = () => {
+        setIsLoggedIn(true)
+ }
+ const handleLogout = () => {
+    setIsLoggedIn(false)
+ }
+ 
+ return (
+    <div>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div>User is {isLoggedIn ? 'Logged in' : 'Logged out'}</div>
+    </div>
+  )
+}
+
+export default LoggedIn
