405de16d9d6dccfc6777f200495d65b1e251ffd6
Travis <travisjb0@gmail.com>
2023-08-11 17:23:13 -0700

    Learned useState Future Value

---
+++ reactTypescriptTutorial/src/components/state/User.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+
+const User = () => {
+  const [user, setUser] = useState<AuthUser | null>(null)
+  
+  const handleLogin = () => {
+    setUser({
+        name: 'Vishwas',
+        email: 'Vishwas@ecample.com',
+    })
+  }
+
+  const handleLogout = () => {
+        setUser(null)
+  }
+  
+  
+    return (
+    <div>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div>User name is {user?.name}</div>
+        <div>User email is{user?.email}</div>
+    </div>
+  )
+}
+
+export default User
